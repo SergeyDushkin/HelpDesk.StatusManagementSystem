60ee252ed0827df1be7e7ebf88a755a606d5bbc8
SergeyDushkin <leirbythe@gmail.com>
2017-01-19 00:18:35 +0300

    update nlog

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,8 @@ namespace servicedesk.StatusManagementSystem
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddNLog();
-            //env.ConfigureNLog("nlog.config");
+            loggerFactory.ConfigureNLog("nlog.config");
+            
             app.UseOwin().UseNancy(x => x.Bootstrapper = new Bootstrapper(Configuration));
         }
     }
